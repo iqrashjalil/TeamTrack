d62e9216a97b59900b78801374d2a34363f7dfa9
Iqrash Jalil <iqrashjalil@gmail.com>
2024-06-28 23:24:44 +0500

    created register and login controllers

---
+++ backend/controllers/auth-controller.js
@@ -0,0 +1,44 @@
+import catchAsyncError from "../middlewares/catchAsyncError.js";
+import User from "../models/user-model.js";
+import sendToken from "../utils/jwtToken.js";
+import { ErrorHandler } from "../utils/error-handler.js";
+
+//* Register
+const register = catchAsyncError(async (req, res, next) => {
+  const { name, email, password, role, profilePicture } = req.body;
+  const userExist = await User.findOne({ email });
+
+  if (userExist) {
+    return next(
+      new ErrorHandler(`User wtih email:${email} Already Exist`, 400)
+    );
+  }
+  const user = await User.create({
+    name,
+    email,
+    password,
+    role,
+    profilePicture,
+  });
+
+  res.status(201).json({
+    success: true,
+    message: "User Registered Successfully",
+    data: user,
+  });
+});
+
+//* Login
+const login = catchAsyncError(async (req, res, next) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email }).select("+password");
+  if (!user) {
+    return next(new ErrorHandler("Invalid Credentials", 401));
+  }
+  const isMatch = await user.comparePassword(password);
+  if (!isMatch) {
+    return next(new ErrorHandler("Invalid Credentials", 401));
+  }
+  sendToken(user, 200, res);
+});
+export default register;
--- backend/models/user-model.js
+++ backend/models/user-model.js
@@ -1,4 +1,4 @@
-import { Schema } from "mongoose";
+import { Schema, Model } from "mongoose";
 
 const userSchema = new Schema(
   {
@@ -27,3 +27,31 @@ const userSchema = new Schema(
   },
   { timestamps: true }
 );
+userSchema.pre("save", async function (next) {
+  const user = this;
+
+  if (!user.isModified("password")) {
+    next();
+  }
+  try {
+    const saltRound = await bcrypt.genSalt(10);
+    const hashed_password = await bcrypt.hash(user.password, saltRound);
+    user.password = hashed_password;
+  } catch (error) {
+    next(error);
+  }
+});
+
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.methods.generateToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET_KEY, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
+
+const User = Model("User", userSchema);
+
+export default User;
