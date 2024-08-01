b295a8c4e9edc7e76804952b438d87393d7dd2c7
Iqrash Jalil <iqrashjalil@gmail.com>
2024-07-31 23:32:51 -0700

    Added Admin Credentials Alert

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import HomeImg from "../images/home.png";
 import picture from "../images/new.png";
 import { useNavigate, useLocation } from "react-router-dom";
@@ -18,6 +18,10 @@ const Home = () => {
   const navigate = useNavigate();
   const { user } = useSelector((state) => state.users);
   const location = useLocation();
+  const [isVisible, setIsVisible] = useState(true);
+  const handleClose = () => {
+    setIsVisible(false);
+  };
 
   useEffect(() => {
     if (location.state && location.state.section) {
@@ -30,6 +34,51 @@ const Home = () => {
   }, [location]);
   return (
     <>
+      {isVisible && (
+        <div
+          id="alert-border-1"
+          className="flex items-center p-4 mb-4 text-blue-800 border-t-4 border-blue-300 bg-blue-50 dark:text-blue-400 dark:bg-gray-800 dark:border-blue-800"
+          role="alert"
+        >
+          <svg
+            className="flex-shrink-0 w-4 h-4"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+          >
+            <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z" />
+          </svg>
+
+          <div className="ms-3 text-sm font-medium">
+            Admin Credentials: Email: admin@gmail.com Password: admin@1
+          </div>
+
+          <button
+            type="button"
+            className="ms-auto -mx-1.5 -my-1.5 bg-blue-50 text-blue-500 rounded-lg focus:ring-2 focus:ring-blue-400 p-1.5 hover:bg-blue-200 inline-flex items-center justify-center h-8 w-8 dark:bg-gray-800 dark:text-blue-400 dark:hover:bg-gray-700"
+            aria-label="Close"
+            onClick={handleClose}
+          >
+            <span className="sr-only">Dismiss</span>
+            <svg
+              className="w-3 h-3"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 14"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+              />
+            </svg>
+          </button>
+        </div>
+      )}
       <section className="p-2">
         <div className="flex flex-col lg:h-[93vh] w-full lg:flex-row lg:justify-center items-center ">
           <div className="h-max lg:w-1/2 lg:pl-28">
@@ -330,7 +379,6 @@ const Home = () => {
           </div>
         </Element>
       </section>
-
       <div className="absolute top-0 w-full h-full bg-white -z-10">
         <div className="absolute bottom-auto left-auto right-0 top-0 h-[500px] w-[500px] -translate-x-[30%] translate-y-[20%] rounded-full bg-[rgba(173,109,244,0.5)] opacity-50 blur-[80px]"></div>
       </div>
